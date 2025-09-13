f3d77d5391f6a651c4d339a2267d0f900e41136e
Krupnova Natalya <krupnova88@mail.ru>
2025-09-13 21:20:36 +0400

    Исправления

--- conftest.py
+++ conftest.py
@@ -5,3 +5,7 @@ import pytest
 def browser_window():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+
+    yield
+
+    browser.quit()
--- test_search.py
+++ test_search.py
@@ -1,16 +1,14 @@
 import pytest
 from selene import browser, be, have, by
 
-def test_ya_search_positive():
 
+def test_ya_search_positive(browser_window):
     browser.open('https://ya.ru')
     browser.element('[name="text"]').should(be.blank).type('qa.guru').press_enter()
     browser.element('html').should(have.text('Курсы тестировщиков'))
 
-def test_ya_search_negative():
-        browser.open('https://ya.ru')
-        browser.element('[name="text"]').should(be.blank).type('burusuzugu').press_enter()
-        browser.element('html').should(have.text('Ничего не нашли'))
 
-
-
+def test_ya_search_negative(browser_window):
+    browser.open('https://ya.ru')
+    browser.element('[name="text"]').should(be.blank).type('burusuzugu').press_enter()
+    browser.element('html').should(have.text('Ничего не нашли'))
